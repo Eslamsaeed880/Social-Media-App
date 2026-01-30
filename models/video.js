c4e1756296ce2ed410ced9a7781b6f2112333a56
Eslam Saeed <eslamsaeed298@gmail.com>
2026-01-30 05:39:35 +0200

    feat: add video model with schema and integrate mongoose-aggregate-paginate-v2

---
+++ models/video.js
@@ -0,0 +1,90 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+
+
+const videoSchema = new Schema({
+    videoFile: {
+        publicId: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    thumbnail: {
+        publicId: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    title: {
+        type: String,
+        required: true,
+        maxLength: 100,
+        trim: true,
+        index: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    shares: {
+        type: Number,
+        default: 0
+    },
+    comments: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'VideoCategory',
+        required: true
+    },
+    isPublished: {
+        type: Boolean,
+        default: false
+    },
+    publisherId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    ageRestriction: {
+        type: String,
+        enum: ['all', '18+'],
+        default: 'all'
+    }
+});
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+videoSchema.index({title: 'text', description: 'text', tags: 'text'});
+
+const videoModel = mongoose.model('Video', videoSchema);
+
+export default videoModel;
