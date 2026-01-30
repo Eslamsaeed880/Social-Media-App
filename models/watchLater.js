f205f57b9e0d5e7b2bbddfaa035c40e0792630b5
Eslam Saeed <eslamsaeed298@gmail.com>
2026-01-30 09:11:29 +0200

    feat: create watch later model with schema and validation

---
+++ models/watchLater.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const watchLaterSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    videos: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Video',
+        required: true
+    }]
+}, { timestamps: true });
+
+const WatchLater = mongoose.model('WatchLater', watchLaterSchema);
+
+export default WatchLater;
