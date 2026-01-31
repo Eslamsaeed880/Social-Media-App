e90208267129c24dc580242aece176fb382e21b1
Eslam Saeed <eslamsaeed298@gmail.com>
2026-01-31 05:04:36 +0200

    feat: add routes for analytics, comment, like, notification, playlist, report, user, video, videoCategory, and watch later

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 connectDb();
 
+app.use(express.json());
+
+// app.use('/api/',);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
