ef5e38d7ea087f042d35aea8fcf9def7f0afe876
Eslam Saeed <eslamsaeed298@gmail.com>
2026-02-14 15:31:44 +0200

    feat: add reply functionality to comments with error handling

--- controllers/comment.js
+++ controllers/comment.js
@@ -2,6 +2,7 @@ import APIError from "../utils/APIError.js";
 import APIResponse from "../utils/APIResponse.js";
 import Comment from "../models/comment.js";
 import Video from "../models/video.js";
+import mongoose from "mongoose"
 
 // @Desc: Create a comment on a video
 // @Route: POST /api/v1/comments/:videoId
@@ -29,6 +30,38 @@ export const createCommentOnVideo = async (req, res, next) => {
 
         return res.status(201).json(new APIResponse(201, 'Comment created successfully', comment));
         
+    } catch (error) {
+        console.log(error);
+        return next(new APIError(500, 'Server error'));
+    }
+}
+
+// @Desc: Reply on a comment
+// @Route: POST /api/v1/comments?commentId=698faf3579a3dda5f68c5ca9
+// @Access: Private
+export const replyOnComment = async (req, res, next) => {
+    try {
+        const { commentId } = req.query;
+        const { content } = req.body;
+
+        const parentComment = await Comment.findById(commentId);
+        if (!parentComment) {
+            return next(new APIError(404, 'Comment not found'));
+        }
+
+        const comment = new Comment({
+            content,
+            videoId: parentComment.videoId,
+            createdBy: req.user.id,
+        });
+        
+        parentComment.replies.push(new mongoose.Types.ObjectId(comment._id));
+
+        await comment.save();
+        await parentComment.save();
+
+        return res.status(201).json(new APIResponse(201, 'Reply created successfully', comment));
+
     } catch (error) {
         console.log(error);
         return next(new APIError(500, 'Server error'));
--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import {
     createCommentOnVideo,
+    replyOnComment,
 } from '../controllers/comment.js';
 import isAuth from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
+router.post("/", isAuth, replyOnComment);
+
 router.post("/:videoId", isAuth, createCommentOnVideo);
+
+
 
 export default router;
