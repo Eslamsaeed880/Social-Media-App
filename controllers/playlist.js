399f7ce1a098f41fcb27457c79be155a8b2d3a86
Eslam Saeed <eslamsaeed298@gmail.com>
2026-02-22 06:14:51 +0200

    feat: add functionality to retrieve all videos in a playlist and update route for adding videos

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -52,7 +52,7 @@ export const getUserPlaylists = async (req, res, next) => {
 }
 
 // Desc: Add a video to a playlist
-// Route: POST /api/v1/playlists/:playlistId/video
+// Route: POST /api/v1/playlists/:playlistId/videos
 // Access: Private
 export const addVideoToPlaylist = async (req, res, next) => {
     try {
@@ -81,6 +81,32 @@ export const addVideoToPlaylist = async (req, res, next) => {
         await playlist.save();
 
         return res.status(200).json(new APIResponse(200, playlist, 'Video added to playlist successfully'));
+    } catch (error) {
+        console.log(error);
+        return next(new APIError(500, 'Server error'));
+    }
+}
+
+// @Desc: Get all videos in a playlist
+// @Route: GET /api/v1/playlists/:playlistId/videos?limit=10&page=1
+// @Access: Private
+export const getPlaylistVideos = async (req, res, next) => {
+    try {
+        const { page = 1, limit = 10 } = req.query;
+        const { playlistId } = req.params;
+        const playlist = await Playlist.findById(playlistId);
+        if (!playlist) {
+            return next(new APIError(404, 'Playlist not found'));
+        }
+        const videos = await Video.find({ _id: { $in: playlist.videos } }).skip((page - 1) * limit).limit(+limit);
+        const totalResults = await Video.countDocuments({ _id: { $in: playlist.videos } });
+
+        return res.status(200).json(new APIResponse(200, { 
+            videos, 
+            totalResults, 
+            currentPage: +page, 
+            totalPages: Math.ceil(totalResults / +limit),
+        }, 'Videos fetched successfully'));
     } catch (error) {
         console.log(error);
         return next(new APIError(500, 'Server error'));
--- routes/playlist.js
+++ routes/playlist.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPlaylist, getUserPlaylists, addVideoToPlaylist } from "../controllers/playlist.js";
+import { createPlaylist, getUserPlaylists, addVideoToPlaylist, getPlaylistVideos } from "../controllers/playlist.js";
 import isAuth, {isLoggedIn} from "../middlewares/isAuth.js";
 
 const router = express.Router();
@@ -8,6 +8,8 @@ router.post("/", isAuth, createPlaylist);
 
 router.get("/:userId/user", isLoggedIn, getUserPlaylists);
 
-router.post("/:playlistId/video", isAuth, addVideoToPlaylist);
+router.post("/:playlistId/videos", isAuth, addVideoToPlaylist);
+
+router.get("/:playlistId/videos", getPlaylistVideos);
 
 export default router;
